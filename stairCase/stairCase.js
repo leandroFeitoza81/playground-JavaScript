67ad0a385a0816d45e5af5a4e63d44463fd14033
Leandro <leandro.feitoza8018@gmail.com>
2021-03-21 16:20:30 -0300

    challenge complete

---
+++ stairCase/stairCase.js
@@ -0,0 +1,20 @@
+function stairCase(number) {
+  let symbol = '#';
+  let inputLine = '';
+  let inputPosition = number;
+
+for (let lineIndex = 0; lineIndex < number; lineIndex += 1) {
+  for (let columnIndex = 1; columnIndex <= number; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+};
+}
+
+stairCase(6)
